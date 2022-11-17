5163775d13c9d4799b5450bd93204df467ec995b
MRiabov <maksymriabov2004@gmail.com>
2022-11-17 13:21:17 +0100

    Added tests. Weird stuff happening, files are considered non-project for whatever reason.

--- src/app/data.service.spec.ts
+++ src/app/data.service.spec.ts
@@ -1,6 +1,7 @@
-import { TestBed } from '@angular/core/testing';
+import {TestBed} from '@angular/core/testing';
 
-import { DataService } from './data.service';
+import {DataService} from './data.service';
+import {Book} from "./model/Book";
 
 describe('DataService', () => {
   let service: DataService;
@@ -13,4 +14,10 @@ describe('DataService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('addNewBook increases the size of the books array', () =>{
+    let sizeStart = service.books.length;
+    service.addNewBook(new Book('123','123',123));
+    expect(service.books.length).toBe(sizeStart+1)
+  })
 });
